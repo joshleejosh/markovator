75071dbe8d74eff615030ff928ee012843efe223
Josh Lee <josh@floor.is>
2017-07-23 08:51:11 -0700

    cleanup setup file

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE.txt') as f:
     license = f.read()
 
 setup(
-    name='Markovator',
+    name='markovator',
     version='0.1.0',
     description='A Markov chain-based text remixer/generator.',
     long_description=readme,
@@ -19,23 +19,23 @@ setup(
     url='https://github.com/joshleejosh/markovator',
     license=license,
     classifiers=[
-        'Development Status :: 3 - Alpha'
-        'License :: OSI Approved :: MIT License'
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.6'
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.2'
-        'Programming Language :: Python :: 3.3'
-        'Programming Language :: Python :: 3.4'
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
-        'Topic :: Artistic Software'
-        'Topic :: Software Development :: Libraries'
-        'Topic :: Software Development :: Libraries :: Python Modules'
-        'Topic :: Text Processing'
-        'Topic :: Text Processing :: Filters'
-        'Topic :: Text Processing :: Linguistic'
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Artistic Software',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Linguistic',
         ],
     packages=find_packages(exclude=('tests', 'docs', 'data'))
 )
